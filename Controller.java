2b16b1a17ad0157e45c598db62375b1b5a117084
NyHanja <nyhanja@student.com>
2026-06-16 12:04:59 +0300

    creation de l'annotation Controller

---
+++ Controller.java
@@ -0,0 +1,10 @@
+package mg.itu.annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public class Controller {
+}
